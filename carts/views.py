79696321432d306d1208421172587822d45a8c7e
Myles Traut <myles.traut1@gmail.com>
2020-08-19 16:22:11 +0200

    updated cart display and added add to cart functionality

--- carts/views.py
+++ carts/views.py
@@ -6,16 +6,21 @@ from .models import Cart
 def cart_home(request):
 	cart_obj, new_obj = Cart.objects.new_or_get(request)
 	
-	return render(request, 'carts/home.html', {})
+	return render(request, 'carts/home.html', {'cart':cart_obj})
 
 def cart_update(request):
-	product_id = 1
-	product_obj = Product.objects.get(id=product_id)
-	cart_obj, new_obj = Cart.objects.new_or_get(request)
-	if product_obj in cart_obj.products.all():
-		cart_obj.products.remove(product_obj)
-	else:
-		cart_obj.products.add(product_obj)
+	product_id = request.POST.get('product_id')
+	if product_id is not None:
+		try:
+			product_obj = Product.objects.get(id=product_id)
+		except Product.DoesNotExist:
+			print("show message to user: Product gone?")
+			return redirect('carts:home')
+		cart_obj, new_obj = Cart.objects.new_or_get(request)
+		if product_obj in cart_obj.products.all():
+			cart_obj.products.remove(product_obj)
+		else:
+			cart_obj.products.add(product_obj)
 
-	#return redirect(product_obj.get_absolute_url())
+		#return redirect(product_obj.get_absolute_url())
 	return redirect('carts:home')
