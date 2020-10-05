2ae944347c2fe06c05a178bbb6f1f6069524a22b
Myles Traut <myles.traut1@gmail.com>
2020-10-05 16:41:58 +0200

    Created billing profile manager

--- carts/views.py
+++ carts/views.py
@@ -37,19 +37,10 @@ def checkout_home(request):
 	if cart_created and Cart.products.count() == 0:
 		return redirect('carts:home')		
 
-	user = request.user
 	login_form = LoginForm()
 	guest_form = GuestForm()
-	billing_profile = None
-	guest_email_id = request.session.get('guest_email_id')
 	
-	if user.is_authenticated:
-		billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
-	elif guest_email_id is not None:
-		guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
-		billing_profile, billing_Guest_profile_created = BillingProfile.objects.get_or_create(email=guest_email_obj.email)
-	else:
-		pass
+	billing_profile, billing_Guest_profile_created = BillingProfile.objects.new_or_get(request)
 
 	if billing_profile is not None:
 		order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
